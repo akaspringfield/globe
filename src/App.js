54464eaa9fa1e4df1d7d7c45e1f3e34004779cda
Aku <akash.tvm@gmail.com>
2022-05-31 21:58:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+//import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data'
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup/>
+      {/* <Final/> */}
+    </div>
+  )
+}
+
+export default App
